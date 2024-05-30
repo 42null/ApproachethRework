59690a09a50049ad6e47b78641ec43d81de1fd57
42null <averyparanoiduser@gmail.com>
2024-05-29 23:12:23 -0500

    Draggable windows

--- Assets/UI/RealObjects/Windows/BaseUIWindow.cs
+++ Assets/UI/RealObjects/Windows/BaseUIWindow.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Approacheth.UI.RealObjects.KindWindows.Prefabs;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Approacheth.UI.RealObjects.KindWindows
 {
 
-    public class BaseUIWindow : MonoBehaviour, IUIWindow
+    public class BaseUIWindow : MonoBehaviour, IUIWindow, IDragHandler, IBeginDragHandler
     {
         public UIWindowFactory.SEGMENTS[] buildChildren = new UIWindowFactory.SEGMENTS[] { UIWindowFactory.SEGMENTS.MADE_FROM_RESOUCES };
         
@@ -20,9 +21,12 @@ namespace Approacheth.UI.RealObjects.KindWindows
         public List<GameObject> segments;
         public GameObject segmentPrefab;
         public GameObject resourceDisplayBoxPrefab;
+
+        public GameObject draggableWindowBar;
         
 
         private List<GameObject> _windowSegments;
+        private Vector2 _dragOffset;
 
         public virtual void Setup(UIConfig uiConfig, SpaceObject spaceObject)
         {
@@ -37,6 +41,10 @@ namespace Approacheth.UI.RealObjects.KindWindows
             //     boxDataStorageScript.symbol.text = resource.resource.symbol;
             //     boxDataStorageScript.amount.text = resource.count.ToString();
             // }
+            
+            
+            // Set up draggable
+            
         }
 
         public UIWindowFactory.SEGMENTS[] GetBuildSegments()
@@ -48,6 +56,30 @@ namespace Approacheth.UI.RealObjects.KindWindows
         {
             Destroy(this.transform.gameObject);
         }
-    }
+        
+        
+        
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                transform as RectTransform, 
+                eventData.position, 
+                eventData.pressEventCamera, 
+                out _dragOffset);
+        }
 
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    transform as RectTransform, 
+                    eventData.position, 
+                    eventData.pressEventCamera, 
+                    out Vector2 localPoint))
+            {
+                Vector2 offsetPosition = localPoint - _dragOffset;
+                transform.localPosition += new Vector3(offsetPosition.x, offsetPosition.y, 0);
+            }
+        }
+    }
+    
 }
